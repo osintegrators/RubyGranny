9db12e4c9a4abadbdd10199e6cc54cea00113a04
Brian Crucitti <bcrucitti@osintegrators.com>
2013-02-15 11:05:23 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyGranny::Application.config.secret_token = '948901d78997b04e01a5fc23a2938eab5d0af712f12f51582467222baf357cbc3a257a792130cb94f7181a4f2d476c301e347dbed08653e5d92a1284b02c4c66'
